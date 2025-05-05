71ed96359e44c5827577697dd858507fd380c9b8
devkrutikaroyals <kruteeka.royalswebtech@gmail.com>
2025-05-05 11:00:55 +0530

    123sdzvxvjjghj

--- src/models/Product.js
+++ src/models/Product.js
@@ -20,7 +20,7 @@ const mongoose = require('mongoose');
 const productSchema = new mongoose.Schema({
   name: { type: String, required: true },
   description: String,
-  price: { type: Number, required: true },
+  price: { type: Number},
   category: String,
   stock: { type: Number, required: true },
   imageUrl: String,
